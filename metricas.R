6e9182e4e2697a1ed1b36eef112a8fad1be11f79
acal8 <acal8@alumni.uv.es>
2026-01-05 00:41:20 +0100

    Actualización metricas.R con función comprobacion (arreglar dim comprimida) y MSE acabado

--- metricas.R
+++ metricas.R
@@ -1,6 +1,44 @@
-
+comprobacion_dim <- function(imagen_orig, imagen_comprimida){
+  ancho_orig <- width(imagen_orig)
+  alto_orig <- height(imagen_orig)
+  
+  ancho_comp <- width(imagen_comp)
+  alto_comp <- height(imagen_comp)
+  
+  if ((ancho_orig != ancho_comp) || (alto_orig != alto_comp)){
+    imagen_comprimida <- resize(imagen_comprimida, size_x = ancho_orig,
+                                size_y = alto_orig)
+  }
+  
+  return(imagen_comprimida)
+}
 
 MSE <- function(imagen_orig, imagen_comprimida){
+  
+  comprobacion_dim(imagen_orig, imagen_comprimida)
+  
+  diferencia <- imagen_orig - imagen_comprimida
+  
+  canales <- imsplit(diferencia, "c")
+  
+  mse_canales <- c()
+  
+  for (canal in canales){
+    matriz <- as.matrix(canal)
+    mse_canales <- c(mse_canales, c(1/(ancho*alto)*sum(matriz^2)))
+  }
+  
+  mse <- sum(mse_canales)/3
+  print(paste('MSE:', mse))
+  
+  return(mse)
+}
+
+# Al elevar al cuadrado, penalizamos los errores grandes (píxeles muy diferentes
+# en la imagen comprimida respecto de la original) y perdonamos los pequeños
+# errores de redondeo.
+
+MAE <- function(imagen_orig, imagen_comprimida){
   
   ancho_orig <- width(imagen_orig)
   alto_orig <- height(imagen_orig)
@@ -9,16 +47,27 @@ MSE <- function(imagen_orig, imagen_comprimida){
   alto_comp <- height(imagen_comp)
   
   if ((ancho_orig != ancho_comp) || (alto_orig != alto_comp)){
-    imagen_orig <- resize(imagen_orig, size_x = ancho_comp, size_y = alto_comp)
+    imagen_comprimida <- resize(imagen_comprimida, size_x = ancho_orig,
+                                size_y = alto_orig)
   }
   
-  mse <- 1/(ancho*alto)
+  diferencia <- imagen_orig - imagen_comprimida
+  
+  canales <- imsplit(diferencia, "c")
+  
+  mse_canales <- c()
+  
+  for (canal in canales){
+    matriz <- as.matrix(canal)
+    mse_canales <- c(mse_canales, c(1/(ancho*alto)*sum(matriz^2)))
+  }
+  
+  mse <- sum(mse_canales)/3
+  print(paste('MSE:', mse))
+  
   return(mse)
 }
 
-# Al elevar al cuadrado, penalizamos los errores grandes (píxeles muy diferentes
-# en la imagen comprimida respecto de la original) y perdonamos los pequeños
-# errores de redondeo.
 
 MAPE <- function(imagen, imagen_comprimida){
   
