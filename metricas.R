643042e462ad3f8afc579cef7db2f5902801b44d
acal8 <acal8@alumni.uv.es>
2026-01-05 01:25:30 +0100

    MAE y RMSE enteras

--- metricas.R
+++ metricas.R
@@ -1,4 +1,5 @@
 comprobacion_dim <- function(imagen_orig, imagen_comprimida){
+  
   ancho_orig <- width(imagen_orig)
   alto_orig <- height(imagen_orig)
   
@@ -15,7 +16,7 @@ comprobacion_dim <- function(imagen_orig, imagen_comprimida){
 
 MSE <- function(imagen_orig, imagen_comprimida){
   
-  comprobacion_dim(imagen_orig, imagen_comprimida)
+  imagen_comprimida <- comprobacion_dim(imagen_orig, imagen_comprimida)
   
   diferencia <- imagen_orig - imagen_comprimida
   
@@ -29,53 +30,53 @@ MSE <- function(imagen_orig, imagen_comprimida){
   }
   
   mse <- sum(mse_canales)/3
-  print(paste('MSE:', mse))
+  # print(paste('MSE:', mse))
   
   return(mse)
 }
 
 # Al elevar al cuadrado, penalizamos los errores grandes (píxeles muy diferentes
 # en la imagen comprimida respecto de la original) y perdonamos los pequeños
-# errores de redondeo.
+# errores de redondeo. Esta métrica no refleja bien la calidad percibida por el
+# ojo humano.
 
 MAE <- function(imagen_orig, imagen_comprimida){
   
-  ancho_orig <- width(imagen_orig)
-  alto_orig <- height(imagen_orig)
-  
-  ancho_comp <- width(imagen_comp)
-  alto_comp <- height(imagen_comp)
-  
-  if ((ancho_orig != ancho_comp) || (alto_orig != alto_comp)){
-    imagen_comprimida <- resize(imagen_comprimida, size_x = ancho_orig,
-                                size_y = alto_orig)
-  }
+  imagen_comprimida <- comprobacion_dim(imagen_orig, imagen_comprimida)
   
   diferencia <- imagen_orig - imagen_comprimida
   
   canales <- imsplit(diferencia, "c")
   
-  mse_canales <- c()
+  mae_canales <- c()
   
   for (canal in canales){
     matriz <- as.matrix(canal)
-    mse_canales <- c(mse_canales, c(1/(ancho*alto)*sum(matriz^2)))
+    mae_canales <- c(mae_canales, c(1/(ancho*alto)*sum(abs(matriz))))
   }
   
-  mse <- sum(mse_canales)/3
-  print(paste('MSE:', mse))
+  mae <- sum(mae_canales)/3
+  # print(paste('MAE:', mae))
   
-  return(mse)
+  return(mae)
 }
+
+RMSE <- function(imagen_orig, imagen_comprimida){
+  
+  mse <- MSE(imagen_orig, imagen_comprimida)
+
+  rsme <- sqrt(mse)
+  
+  return(rsme)
+}
+
+# Esta métrica es útil para interpretar el error en términos de magnitud de píxeles.
+
 
 
 MAPE <- function(imagen, imagen_comprimida){
   
-  ancho_orig <- width(imagen_orig)
-  alto_orig <- height(imagen_orig)
-  
-  ancho_comp <- width(imagen_comp)
-  alto_comp <- height(imagen_comp)
+  imagen_comprimida <- comprobacion_dim(imagen_orig, imagen_comprimida)
   
   
   return(mape)
